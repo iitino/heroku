82166a41fc1f9825a6bafaf1101edc259ab9f8d8
Nakama <kd1180043@st.kobedenshi.ac.jp>
2018-01-23 14:41:44 +0900

    stocksのバグを解消

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -66,7 +66,8 @@ class StocksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_stock
-      @stock = Stock.find(current_user.stock.id)
+      # @stock = Stock.find ( [current_user.stock.ids] )
+      @stock = Stock.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
